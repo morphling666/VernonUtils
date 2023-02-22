57dc81ea33f28cf8f56b71551e0a9a040de2f6ce
morphling666 <1282503538@qq.com>
2023-02-23 02:00:31 +0800

    binary and json serializer and deserializer

--- reflection/serialization.h
+++ reflection/serialization.h
@@ -2,6 +2,7 @@
 #define SERIALIZATION_H
 
 #include <fstream>
+#include <iostream>
 #include <iterator>
 #include <json/json.h>
 #include <list>
@@ -17,7 +18,9 @@
 namespace Vernon {
 
 class BinarySerializer;
+class BinaryDeserializer;
 class JsonSerializer;
+class JsonDeserializer;
 
 #define BASIC_TYPE_SERIALIZE(Type)                                                                                     \
     template <> void serialize(BinarySerializer &s, Type &obj) {                                                       \
@@ -31,7 +34,14 @@ class JsonSerializer;
         val[name] = ret;                                                                                               \
     }
 
-#define BASIC_TYPE_DESERIALIZE(Type)
+#define BASIC_TYPE_DESERIALIZE(Type)                                                                                   \
+    template <> void deserialize(BinaryDeserializer &s, Type &obj) {                                                   \
+        memcpy(&obj, s.str().data(), sizeof(Type));                                                                    \
+        s.str() = s.str().substr(sizeof(Type));                                                                        \
+    }                                                                                                                  \
+    template <> void deserialize(Json::Value &val, const std::string &name, Type &obj) {                               \
+        memcpy(&obj, val[name].asString().data(), sizeof(Type));                                                       \
+    }
 
 #define BASIC_TYPE_SERIALIZE_AND_DESERIALIZE(Type)                                                                     \
     BASIC_TYPE_SERIALIZE(Type)                                                                                         \
@@ -152,6 +162,7 @@ public:
         *this << tmp_key;
         return *this << tmp_value;
     }
+    void reset() { os.str(""); }
     std::ostringstream &outStream() { return os; }
     std::string str() { return os.str(); }
 
@@ -159,9 +170,124 @@ private:
     std::ostringstream os;
 };
 
+template <typename SerializableType> void deserialize(BinaryDeserializer &s, SerializableType &obj) {
+    obj.deserialize(s);
+}
+
+template <typename SerializableType> void deserialize(BinaryDeserializer &s, SerializableType *ptr) {
+    ptr->deserialize(s);
+}
+
+template <typename SerializableType> void deserialize(BinaryDeserializer &s, std::vector<SerializableType> &obj) {
+    int len = 0;
+    deserialize(s, len);
+    for (int i = 0; i < len; ++i) {
+        SerializableType tmp;
+        deserialize(s, tmp);
+        obj.emplace_back(tmp);
+    }
+}
+
+template <typename SerializableType> void deserialize(BinaryDeserializer &s, std::list<SerializableType> &obj) {
+    int len = 0;
+    deserialize(s, len);
+    for (int i = 0; i < len; ++i) {
+        SerializableType tmp;
+        deserialize(s, tmp);
+        obj.emplace_back(tmp);
+    }
+}
+
+template <typename SerializableType> void deserialize(BinaryDeserializer &s, std::set<SerializableType> &obj) {
+    std::vector<SerializableType> tmp;
+    deserialize(s, tmp);
+    for (auto &elem : tmp)
+        obj.insert(elem);
+}
+
+template <typename SerializableTypeA, typename SerializableTypeB>
+void deserialize(BinaryDeserializer &s, std::map<SerializableTypeA, SerializableTypeB> &obj) {
+    std::vector<SerializableTypeA> tmp_key;
+    std::vector<SerializableTypeB> tmp_value;
+    deserialize(s, tmp_key);
+    deserialize(s, tmp_value);
+    int size = tmp_key.size();
+    for (int i = 0; i < size; ++i) {
+        obj.insert(std::make_pair(tmp_key[i], tmp_value[i]));
+    }
+}
+
+template <typename SerializableTypeA, typename SerializableTypeB>
+void deserialize(BinaryDeserializer &s, std::unordered_map<SerializableTypeA, SerializableTypeB> &obj) {
+    std::vector<SerializableTypeA> tmp_key;
+    std::vector<SerializableTypeB> tmp_value;
+    deserialize(s, tmp_key);
+    deserialize(s, tmp_value);
+    int size = tmp_key.size();
+    for (int i = 0; i < size; ++i) {
+        obj.insert(std::make_pair(tmp_key[i], tmp_value[i]));
+    }
+}
+
 class BinaryDeserializer {
 public:
-    BinaryDeserializer() {}
+    BinaryDeserializer(const std::string &str) : binary(str) {}
+    template <typename SerializableType> void operator>>(SerializableType &obj) { deserialize(*this, obj); }
+    template <typename SerializableType> void operator>>(SerializableType *obj) { deserialize(*this, obj); }
+    template <typename SerializableType> void operator>>(std::vector<SerializableType> &obj) {
+        int len = obj.size();
+        deserialize(*this, len);
+
+        for (int i = 0; i < len; ++i) {
+            SerializableType tmp;
+            deserialize(*this, tmp);
+            obj.emplace_back(tmp);
+        }
+    }
+    template <typename SerializableType> void operator>>(std::list<SerializableType> &obj) {
+        int len = obj.size();
+        deserialize(*this, len);
+
+        for (int i = 0; i < len; ++i) {
+            SerializableType tmp;
+            deserialize(*this, tmp);
+            obj.emplace_back(tmp);
+        }
+    }
+    template <typename SerializableType> void operator>>(std::set<SerializableType> &obj) {
+        std::vector<SerializableType> tmp;
+        *this >> tmp;
+        for (auto &elem : tmp)
+            obj.insert(elem);
+    }
+
+    template <typename SerializableTypeA, typename SerializableTypeB>
+    void operator>>(std::map<SerializableTypeA, SerializableTypeB> &obj) {
+        std::vector<SerializableTypeA> tmp_key;
+        std::vector<SerializableTypeB> tmp_value;
+        *this >> tmp_key;
+        *this >> tmp_value;
+        int size = tmp_key.size();
+        for (int i = 0; i < size; ++i) {
+            obj.insert(std::make_pair(tmp_key[i], tmp_value[i]));
+        }
+    }
+    template <typename SerializableTypeA, typename SerializableTypeB>
+    void operator>>(std::unordered_map<SerializableTypeA, SerializableTypeB> &obj) {
+        std::vector<SerializableTypeA> tmp_key;
+        std::vector<SerializableTypeB> tmp_value;
+        *this >> tmp_key;
+        *this >> tmp_value;
+        int size = tmp_key.size();
+        for (int i = 0; i < size; ++i) {
+            obj.insert(std::make_pair(tmp_key[i], tmp_value[i]));
+        }
+    }
+    void reset(const std::string &str) { binary = str; }
+    std::string &str() { return binary; }
+
+private:
+    std::string binary;
 };
 
 template <typename SerializableType> void serialize(Json::Value &val, const std::string &name, SerializableType &obj) {
@@ -271,7 +397,12 @@ public:
     JsonSerializer &transferToJson(const std::string &name, std::set<SerializableType> &obj) {
         std::vector<SerializableType> tmp;
         std::copy(obj.begin(), obj.end(), std::back_inserter(tmp));
-        return transferToJson(name, tmp);
+        serialize(root, name, tmp);
+        std::ofstream file_stream(filename, std::ios::out | std::ios::app);
+        file_stream << json_writer.write(root);
+        file_stream.close();
+        root.clear();
+        return *this;
     }
     template <typename SerializableTypeA, typename SerializableTypeB>
     JsonSerializer &transferToJson(const std::string &name, std::map<SerializableTypeA, SerializableTypeB> &obj) {
@@ -282,9 +413,13 @@ public:
             tmp_key.push_back(it->first);
             tmp_value.push_back(it->second);
         }
-
-        transferToJson(name + "_key", tmp_key);
-        return transferToJson(name + "value", tmp_value);
+        serialize(root, name + "_key", tmp_key);
+        serialize(root, name + "_value", tmp_value);
+        std::ofstream file_stream(filename, std::ios::out | std::ios::app);
+        file_stream << json_writer.write(root);
+        file_stream.close();
+        root.clear();
+        return *this;
     }
     template <typename SerializableTypeA, typename SerializableTypeB>
     JsonSerializer &transferToJson(const std::string &name,
@@ -296,23 +431,100 @@ public:
             tmp_key.push_back(it->first);
             tmp_value.push_back(it->second);
         }
-
-        transferToJson(name + "_key", tmp_key);
-        return transferToJson(name + "_value", tmp_value);
+        serialize(root, name + "_key", tmp_key);
+        serialize(root, name + "_value", tmp_value);
+        std::ofstream file_stream(filename, std::ios::out | std::ios::app);
+        file_stream << json_writer.write(root);
+        file_stream.close();
+        root.clear();
+        return *this;
+    }
+    void reset() {
+        std::ofstream file_stream(filename, std::ios::out);
+        file_stream.close();
     }
 
 private:
-    Json::Value root;
     std::string filename;
+    Json::Value root;
     Json::StyledWriter json_writer;
 };
 
-class JasonDeserializer {
+template <typename SerializableType>
+void deserialize(Json::Value &val, const std::string &name, SerializableType &obj) {
+    obj.deserialize(val[name]);
+}
+
+template <typename SerializableType>
+void deserialize(Json::Value &val, const std::string &name, std::vector<SerializableType> &obj) {
+    int len = 0;
+    deserialize(val[name], "size", len);
+    for (int i = 0; i < len; ++i) {
+        SerializableType tmp;
+        std::string data = "data_" + std::to_string(i);
+        deserialize(val[name], data, tmp);
+        obj.emplace_back(tmp);
+    }
+}
+
+class JsonDeserializer {
 public:
-    JasonDeserializer(const std::string &name) { filename = name + ".json"; }
+    JsonDeserializer(const std::string &name) { filename = name + ".json"; }
+    template <typename SerializableType> void transferToObject(const std::string &name, SerializableType &obj) {
+        std::ifstream file_stream(filename, std::ios::in);
+        json_reader.parse(file_stream, root);
+        deserialize(root, name, obj);
+        file_stream.close();
+    }
+    template <typename SerializableType>
+    void transferToObject(const std::string &name, std::vector<SerializableType> &obj) {
+        std::ifstream file_stream(filename, std::ios::in);
+        json_reader.parse(file_stream, root);
+        int len = 0;
+        deserialize(root[name], "size", len);
+        for (int i = 0; i < len; ++i) {
+            SerializableType tmp;
+            std::string data = "data_" + std::to_string(i);
+            deserialize(root[name], data, tmp);
+            obj.emplace_back(tmp);
+        }
+        file_stream.close();
+    }
+    template <typename SerializableTypeA, typename SerializableTypeB>
+    void transferToObject(const std::string &name, std::map<SerializableTypeA, SerializableTypeB> &obj) {
+        std::ifstream file_stream(filename, std::ios::in);
+        json_reader.parse(file_stream, root);
+
+        std::vector<SerializableTypeA> tmp_key;
+        std::vector<SerializableTypeB> tmp_value;
+        deserialize(root, name + "_key", tmp_key);
+        deserialize(root, name + "_value", tmp_value);
+        int size = tmp_key.size();
+        for (int i = 0; i < size; ++i) {
+            obj.insert(std::make_pair(tmp_key[i], tmp_value[i]));
+        }
+        file_stream.close();
+    }
+    template <typename SerializableTypeA, typename SerializableTypeB>
+    void transferToObject(const std::string &name, std::unordered_map<SerializableTypeA, SerializableTypeB> &obj) {
+        std::ifstream file_stream(filename, std::ios::in);
+        json_reader.parse(file_stream, root);
+
+        std::vector<SerializableTypeA> tmp_key;
+        std::vector<SerializableTypeB> tmp_value;
+        deserialize(root, name + "_key", tmp_key);
+        deserialize(root, name + "_value", tmp_value);
+        int size = tmp_key.size();
+        for (int i = 0; i < size; ++i) {
+            obj.insert(std::make_pair(tmp_key[i], tmp_value[i]));
+        }
+        file_stream.close();
+    }
 
 private:
     std::string filename;
+    Json::Value root;
+    Json::Reader json_reader;
 };
 
 BASIC_TYPE_SERIALIZE_AND_DESERIALIZE(char)
--- tests/serialize_test.cpp
+++ tests/serialize_test.cpp
@@ -12,7 +12,18 @@ struct A {
     }
     virtual void serialize(Json::Value& val)
     {
-        val["a"] = a;
+        Vernon::serialize(val, "a", a);
+    }
+    virtual void deserialize(Vernon::BinaryDeserializer& s)
+    {
+        s >> a;
+    }
+    virtual void deserialize(Json::Value& val)
+    {
+        Vernon::deserialize(val, "a", a);
+    }
+    virtual void print() {
+        std::cout << "a = "<< a << std::endl;
     }
     int a;
 };
@@ -29,43 +40,115 @@ struct B: public A {
     void serialize(Json::Value& val) override
     {
         A::serialize(val);
-        val["b"] = b;
+        Vernon::serialize(val, "b", b);
+    }
+    void deserialize(Vernon::BinaryDeserializer& s) override
+    {
+        A::deserialize(s);
+        s >> b;
+    }
+    void deserialize(Json::Value& val) override
+    {
+        A::deserialize(val);
+        Vernon::deserialize(val, "b", b);
+    }
+    void print() override {
+        A::print();
+        std::cout << "b = "<< b << std::endl;
     }
     int a;
     int b;
 };
 
 int main() {
+    // 1. binary serializer and deserializer test
     // serialize 2-level stl vector
     std::vector<std::vector<int>> vector_level_2(2);
     vector_level_2[0] = std::vector<int>{12, 3, 2};
     vector_level_2[1] = std::vector<int>{1, 3, 33, 22};
-    Vernon::BinarySerializer ss;
-    ss << vector_level_2;
-    std::cout<<ss.str().size()<<std::endl;
+    Vernon::BinarySerializer serializer;
+    serializer << vector_level_2;
+    std::cout<<"binary size = "<<serializer.str().size()<<std::endl;
+    // deserialize 2-level stl vector
+    Vernon::BinaryDeserializer deserialier(serializer.str());
+    std::vector<std::vector<int>> vector_level_2_new;
+    deserialier >> vector_level_2_new;
+    for (int i = 0; i < (int)vector_level_2_new.size(); ++i) {
+        for (int j = 0; j < (int)vector_level_2_new[i].size(); ++j)
+            std::cout << vector_level_2_new[i][j] << " ";
+        std::cout<<std::endl;
+    }
     // serialize pointer
+    serializer.reset();
     A *a=new A();
-    ss << a;
-    std::cout<<ss.str().size()<<std::endl;
+    serializer << a;
+    std::cout<<"binary size = "<<serializer.str().size()<<std::endl;
+    // deserialize base class
+    deserialier.reset(serializer.str());
+    A new_a;
+    deserialier >> new_a;
+    new_a.print();
     // serialize derive class
+    serializer.reset();
     B b;
     A &base_b=b;
-    ss << base_b;
-    std::cout<<ss.str().size()<<std::endl;
-    Vernon::JsonSerializer test_json("test_serial");
+    serializer << base_b;
+    std::cout<<"binary size = "<<serializer.str().size()<<std::endl;
+    // deserialize derive class
+    deserialier.reset(serializer.str());
+    B new_b;
+    deserialier >> new_b;
+    new_b.print();
+    // 2. json serializer and deserializer test
+    Vernon::JsonSerializer json_serializer("test_serial");
     // serialize 3-level stl vector
     std::vector<std::vector<std::vector<int>>> vector_level_3(2);
     vector_level_3[0] = vector_level_2;
     vector_level_3[1] = vector_level_2;
+    json_serializer.transferToJson("vector_level_3", vector_level_3);
+    // deserialize 3-level stl vector
+    Vernon::JsonDeserializer json_deserializer("test_serial");
+    std::vector<std::vector<std::vector<int>>> vector_level_3_new;
+    json_deserializer.transferToObject("vector_level_3", vector_level_3_new);
+    for (int i = 0; i < (int)vector_level_3_new.size(); ++i) {
+        for (int j = 0; j < (int)vector_level_3_new[i].size(); ++j) {
+            for (int k = 0; k < (int)vector_level_3_new[i][j].size(); ++k) {
+                std::cout << vector_level_3_new[i][j][k] << " ";
+            }
+            std::cout<<std::endl;
+        }
+        std::cout<<std::endl;
+    }
     // serialize unordered_map
+    json_serializer.reset();
     std::unordered_map<int, std::vector<int>> hash_level_2;
     hash_level_2[1] = std::vector<int>{1, 2, 4};
     hash_level_2[8] = std::vector<int>{3, 44, 8};
-    int name = 1;
-    int k = 10;
-    test_json.transferToJson("name", name).transferToJson("k", k).transferToJson("A", *a);
-    test_json.transferToJson("vector_level_2", vector_level_2).transferToJson("B", base_b);
-    test_json.transferToJson("vector_level_3", vector_level_3);
-    test_json.transferToJson("hash_level_2", hash_level_2);
+    json_serializer.transferToJson("hash_level_2", hash_level_2);
+    // deserialize unordered_map
+    std::unordered_map<int, std::vector<int>> hash_level_2_new;
+    json_deserializer.transferToObject("hash_level_2", hash_level_2_new);
+    // here
+    for (auto it = hash_level_2_new.begin(); it != hash_level_2_new.end(); ++it) {
+        int key = it->first;
+        std::cout<<"key="<<key<<", value=";
+        for (int j = 0; j < (int)it->second.size(); ++j) {
+            std::cout << it->second[j] << " ";
+        }
+        std::cout<<std::endl;
+    }
+    // serialize base class
+    json_serializer.reset();
+    json_serializer.transferToJson("A", *a);
+    A new_json_a;
+    json_deserializer.transferToObject("A", new_json_a);
+    new_json_a.print();
+    // deserialize derive class
+    json_serializer.reset();
+    json_serializer.transferToJson("B", base_b);
+    B new_json_b;
+    json_deserializer.transferToObject("B", new_json_b);
+    new_json_b.print();
+
 	return 0;
 }
